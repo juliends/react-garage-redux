6889985b12e442f7fa8252a301df6a06bbfc3e5f
Julien Da Silva <julien.da.silva@hotmail.com>
2017-09-28 11:32:17 +0200

    initial state

--- src/index.jsx
+++ src/index.jsx
@@ -12,12 +12,18 @@ import About from './components/about';
 import '../assets/stylesheets/application.scss';
 
 // State and reducers
+import carsReducer from './reducers/cars_reducer';
 const initialState = {
-  changeMe: null
+  cars: [
+    { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
+    { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
+    { id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
+    { id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
+  ]
 };
 
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  cars: carsReducer
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
